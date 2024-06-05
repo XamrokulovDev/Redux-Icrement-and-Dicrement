a2aa9e3e12901bef698bee5e0b94f927981ec050
Xamrokulovdev <frontenduchun@gmail.com>
2024-06-05 13:19:53 +0500

    the project is finished

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,28 @@
 import { useSelector, useDispatch } from 'react-redux'
-import { decrement, increment } from './store/Slices/Slice'
+import { decrement, increment, incrementByAmount } from './store/Slices/Slice'
 import { useEffect, useState } from 'react'
 
 const App = () => {
   const count = useSelector((state) => state.counter.value);
-  const [color,setColor]= useState('green');
+  const [color, setColor] = useState('green');
+  const [incrementAmount, setIncrementAmount] = useState(0);
   const dispatch = useDispatch();
   
-  useEffect(()=>{
-    if(count < 0){
+  useEffect(() => {
+    if (count < 0) {
       setColor('red')
-    }else if(count > 0 ){
+    } else if (count > 0) {
       setColor('green')
-    }
-    else{
+    } else {
       setColor('black')
     }
-  })
+  }, [count]);
   
   return (
     <div className='flex flex-col justify-between items-center border-2 rounded-3xl w-[30vw] h-[40vh] py-[5vh]'>
       <h1 style={{color:color}} className='text-[150px]'>{count}</h1>
       <div className=''>
-        <div className='flex items-center gap-10'>
+        <div className='flex items-center gap-3'>
           <button
             className='text text-white bg-green-600 py-2 px-5 rounded-lg font-semibold'
             aria-label="Increment value"
@@ -37,6 +37,21 @@ const App = () => {
           >
             Decrement
           </button>
+          <div className='flex items-center justify-between gap-3'>
+            <input
+              type='number'
+              value={incrementAmount}
+              onChange={(e) => setIncrementAmount(Number(e.target.value))}
+              className='text-center w-10 py-2 border rounded-lg outline-none'
+            />
+            <button
+              className='text text-white bg-blue-600 py-2 px-5 rounded-lg font-semibold'
+              aria-label="Increment by amount"
+              onClick={() => dispatch(incrementByAmount(incrementAmount))}
+            >
+              Increment by Amount
+            </button>
+          </div>
         </div>
       </div>
     </div>
